230b7f5b41742800fc713b6ead25a03d1711ad06
LSM <dev@localsearchmasters.com>
2018-12-13 14:23:58 -0600

    views for backpack 3.5

--- src/resources/views/run.blade.php
+++ src/resources/views/run.blade.php
@@ -17,40 +17,54 @@
         <div class="col-md-8 col-md-offset-2">
             <!-- Default box -->
             @if ($crud->hasAccess('list'))
-                <a href="{{ url($crud->route) }}"><i class="fa fa-angle-double-left"></i> {{ trans('backpack::crud.back_to_all') }} <span>{{ $crud->entity_name_plural }}</span></a><br><br>
+                <a href="{{ url($crud->route) }}" class="hidden-print"><i class="fa fa-angle-double-left"></i> {{ trans('backpack::crud.back_to_all') }} <span>{{ $crud->entity_name_plural }}</span></a><br><br>
             @endif
+
 
             @include('crud::inc.grouped_errors')
             <form method="post"
-                action="{{ url($crud->route."/$id/run") }}"
-                >
-            {!! csrf_field() !!}
-            <div class="box">
+                  action="{{ url($crud->route."/$id/run") }}"
+            >
+                {!! csrf_field() !!}
+                {!! method_field('PUT') !!}
+                <div class="box">
+                    <div class="box-header with-border">
+                    @if ($crud->model->translationEnabled())
+                        <!-- Single button -->
+                            <div class="btn-group pull-right">
+                                <button type="button" class="btn btn-sm btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    {{trans('backpack::crud.language')}}: {{ $crud->model->getAvailableLocales()[$crud->request->input('locale')?$crud->request->input('locale'):App::getLocale()] }} <span class="caret"></span>
+                                </button>
+                                <ul class="dropdown-menu">
+                                    @foreach ($crud->model->getAvailableLocales() as $key => $locale)
+                                        <li><a href="{{ url($crud->route.'/'.$entry->getKey().'/edit') }}?locale={{ $key }}">{{ $locale }}</a></li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                            <h3 class="box-title" style="line-height: 30px;">{{ trans('backpack::crud.edit') }}</h3>
+                        @else
+                            <h3 class="box-title">{{ trans('backpack::crud.edit') }}</h3>
+                        @endif
+                    </div>
+                    <div class="box-body row display-flex-wrap" style="display: flex;flex-wrap: wrap;">
+                        <!-- load the view from the application if it exists, otherwise load the one in the package -->
+                        @if(view()->exists('vendor.backpack.crud.form_content'))
+                            @include('vendor.backpack.crud.form_content', ['fields' => $fields, 'action' => 'edit'])
+                        @else
+                            @include('crud::form_content', ['fields' => $fields, 'action' => 'edit'])
+                        @endif
+                    </div><!-- /.box-body -->
+                    <div class="box-footer">
 
-                <div class="box-header with-border">
-                    <h3 class="box-title">Configuration</h3>
-                </div>
-                <div class="box-body row">
-                    <!-- load the view from the application if it exists, otherwise load the one in the package -->
-                    @if(view()->exists('vendor.backpack.crud.form_content'))
-                        @include('vendor.backpack.crud.form_content', [ 'fields' => $fields])
-                    @else
-                        @include('crud::form_content', [ 'fields' => $fields])
-                    @endif
-                </div><!-- /.box-body -->
-                <div class="box-footer">
+                        <button type="submit" class="btn btn-success">
+                            <span class="fa fa-play" role="presentation" aria-hidden="true"></span> &nbsp;
+                            <span>Run</span>
+                        </button>
 
-                    <button type="submit" class="btn btn-success">
-                        <span class="fa fa-play" role="presentation" aria-hidden="true"></span> &nbsp;
-                        <span>Run</span>
-                    </button>
-
-                    <a href="{{ url($crud->route) }}" class="btn btn-default"><span class="fa fa-ban"></span> &nbsp;{{ trans('backpack::crud.cancel') }}</a>
-                </div><!-- /.box-footer-->
-
-            </div><!-- /.box -->
+                        <a href="{{ url($crud->route) }}" class="btn btn-default"><span class="fa fa-ban"></span> &nbsp;{{ trans('backpack::crud.cancel') }}</a>
+                    </div><!-- /.box-footer-->
+                </div><!-- /.box -->
             </form>
         </div>
     </div>
-
 @endsection
