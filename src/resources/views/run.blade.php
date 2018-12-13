e7f882a6557368d7b0971cebd862c7f7beb49437
LSM <dev@localsearchmasters.com>
2018-12-13 15:19:47 -0600

    new view

--- src/resources/views/run.blade.php
+++ src/resources/views/run.blade.php
@@ -13,39 +13,18 @@
 @endsection
 
 @section('content')
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <!-- Default box -->
-            @if ($crud->hasAccess('list'))
-                <a href="{{ url($crud->route) }}" class="hidden-print"><i class="fa fa-angle-double-left"></i> {{ trans('backpack::crud.back_to_all') }} <span>{{ $crud->entity_name_plural }}</span></a><br><br>
-            @endif
-
-
+    @if ($crud->hasAccess('list'))
+        <a href="{{ starts_with(URL::previous(), url($crud->route)) ? URL::previous() : url($crud->route) }}" class="hidden-print"><i class="fa fa-angle-double-left"></i> {{ trans('backpack::crud.back_to_all') }} <span>{{ $crud->entity_name_plural }}</span></a>
+    @endif
+    <div class="row m-t-20">
+        <div class="{{ $crud->getEditContentClass() }}">
             @include('crud::inc.grouped_errors')
             <form method="post"
                   action="{{ url($crud->route."/$id/run") }}"
             >
                 {!! csrf_field() !!}
                 {!! method_field('PUT') !!}
-                <div class="box">
-                    <div class="box-header with-border">
-                    @if ($crud->model->translationEnabled())
-                        <!-- Single button -->
-                            <div class="btn-group pull-right">
-                                <button type="button" class="btn btn-sm btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    {{trans('backpack::crud.language')}}: {{ $crud->model->getAvailableLocales()[$crud->request->input('locale')?$crud->request->input('locale'):App::getLocale()] }} <span class="caret"></span>
-                                </button>
-                                <ul class="dropdown-menu">
-                                    @foreach ($crud->model->getAvailableLocales() as $key => $locale)
-                                        <li><a href="{{ url($crud->route.'/'.$entry->getKey().'/edit') }}?locale={{ $key }}">{{ $locale }}</a></li>
-                                    @endforeach
-                                </ul>
-                            </div>
-                            <h3 class="box-title" style="line-height: 30px;">{{ trans('backpack::crud.edit') }}</h3>
-                        @else
-                            <h3 class="box-title">{{ trans('backpack::crud.edit') }}</h3>
-                        @endif
-                    </div>
+                <div class="col-md-12">
                     <div class="row display-flex-wrap">
                         <!-- load the view from the application if it exists, otherwise load the one in the package -->
                         @if(view()->exists('vendor.backpack.crud.form_content'))
@@ -54,7 +33,7 @@
                             @include('crud::form_content', ['fields' => $fields, 'action' => 'edit'])
                         @endif
                     </div><!-- /.box-body -->
-                    <div class="box-footer">
+                    <div class="">
 
                         <button type="submit" class="btn btn-success">
                             <span class="fa fa-play" role="presentation" aria-hidden="true"></span> &nbsp;
